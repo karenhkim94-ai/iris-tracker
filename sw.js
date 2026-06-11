be20b1fdc9c1881d8807aba04d4fc4be50324799
karenhkim94-ai <karenhkim94@gmail.com>
2026-06-10 17:28:52 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,6 +9,16 @@ self.addEventListener("push", e => {
   );
 });
 
+self.addEventListener("message", e => {
+  if (e.data && e.data.type === "NOTIFY") {
+    self.registration.showNotification(e.data.title || "Iris 🐾", {
+      body: e.data.body || "",
+      icon: "/iris-tracker/icon.png",
+      badge: "/iris-tracker/icon.png",
+    });
+  }
+});
+
 self.addEventListener("notificationclick", e => {
   e.notification.close();
   e.waitUntil(clients.openWindow("/iris-tracker/"));
